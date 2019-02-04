4958657ca406419d20a82c13155e1f802e5d34f0
MaximRodkin <flash.l2@yandex.ru>
2019-02-04 13:07:10 +0300

    add Snake.sln

---
+++ Snake/FoodCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Snake
+{
+    class FoodCreator
+    {
+        private int mapWidth;
+        private int mapHeight;
+        private char sym;
+
+        private Random random = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, char sym)
+        {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            this.sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = random.Next(2, mapWidth - 2);
+            int y = random.Next(2, mapHeight - 2);
+            return new Point(x, y, sym);
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+	
+            Snake snake = new Snake(new Point(4, 5, 'o'), 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+        }
+    }
+}
